9b0cbe7210a568c7974febd6e195650fdd52b7ab
Ganter <alexgantera@gmail.com>
2021-01-18 19:59:41 +0300

    Curl

--- install.sh
+++ install.sh
@@ -714,8 +714,9 @@ do
     :
 done
 if [[ $int == 1 ]]; then
-
   wget -P /mnt https://raw.githubusercontent.com/alexgantera/arch/branch-xfce/chroot.sh
+  curl -LO https://raw.githubusercontent.com/alexgantera/arch/branch-xfce/chroot.sh
+  mv chroot.sh /mnt
   chmod +x /mnt/chroot.sh
   echo 'первый этап готов '
   echo 'ARCH-LINUX chroot'
